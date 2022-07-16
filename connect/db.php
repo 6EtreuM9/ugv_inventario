6181af896440e7dbf4b878c463372d847ee839ee
6EtreuM9 <rigel.wo@gmail.com>
2022-07-16 01:35:17 -0500

    mysqli_connect_error

--- connect/db.php
+++ connect/db.php
@@ -2,7 +2,7 @@
 
 $enlace = mysqli_connect('localhost', 'Rigel', '1234');
 if (!$enlace) {
-    die('No pudo conectarse: ' . mysqli_error());
+    die('No pudo conectarse: ' . mysqli_connect_error());
 }
 echo 'Conectado satisfactoriamente <br>';
 //mysqli_close($enlace);
